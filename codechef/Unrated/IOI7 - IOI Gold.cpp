0c0ad3309d0f75fa3de54861192137b2580652c7
Shikhar <shikhar0538@gmail.com>
2026-08-19 20:01:58 +0530

    Add IOI Gold (Unrated)

---
+++ codechef/Unrated/IOI7 - IOI Gold.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,g;
+	cin >> n >>g;
+	if(n >= g){
+	    cout << "YES\n";
+	}
+	else{
+	    cout << "NO\n";
+	}
+
+}
