a2f39beb0a06efe18ba34f6126fd9c07c9703589
Shikhar <shikhar0538@gmail.com>
2026-08-23 01:43:56 +0530

    Add Mex and Max (1500)

--- codechef/Unrated/MEXMAX7 - Mex and Max.cpp
+++ codechef/Unrated/MEXMAX7 - Mex and Max.cpp
@@ -1,21 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int mod = 998244353;
 
-int power(int y){
-    int ans = 1;
-    int x = 2;
-    while(y > 0){
-        if(y & 1){
-            ans = (1LL * ans * x) % mod;
-            
-            
-        }
-        x = (1LL * x * x)% mod;
-            y >>= 1;
+#define ll long long
+const int mod = 998244353;
+int dp[101][101];
+
+
+int f(int pos, int mex, int max_pos, vector<int>&arr){
+    if(pos == max_pos){
+        return abs(mex- arr[max_pos]) <= 1;
     }
-    return ans;
+    
+    int &ans = dp[pos][mex];
+    if(ans  != -1){
+        return ans;
+    }
+    
+    int ans1 = f(pos+1, mex, max_pos,arr);
+    int ans2;
+    
+    if(arr[pos] == mex){
+        ans2 = f(pos+1, mex+1, max_pos, arr);
+    }
+    else{
+        ans2 = f(pos+1, mex, max_pos, arr);
+    }
+    ans = (ans1+ans2)% mod;
+    return dp[pos][mex] = ans;
 }
+
+
 
 int main() {
 	// your code goes here
@@ -24,65 +38,19 @@ int main() {
 	while(t--){
 	    int n;
 	    cin >> n;
-	    vector<int>a(n);
-	    
-	    map<int,int>mp;
-	    for(int i = 0;i < n; i++){
-	        cin >> a[i];
-	        mp[a[i]]++;
+	    vector<int>arr(n);
+	    for(int i = 0;i <n; i++){
+	        cin >> arr[i];
 	    }
-	   // |mex(b) - max(b)| <= 1
-	   //minimum non negative integer not present in b,
-	   //maximum non negative integer not present in b
-	   //x-y ==0
-	   //when x == y
-	   //x-y == 1
-	   //jab maximum element jo nahi hai wo minimum element jo hai usse ek jyada ho
-	  
-	   // mexB = max B galat
-	   //mex B = max B + 1
-	   //max B = mex B + 1
-	   map<int,int>p;
-	   
-	   int answer = 1;
-	   for(int i = 0;i <= n +1 ;i++){
-	       int x = (power(mp[i])-1 + mod) % mod;
-	       answer = (1LL * x * answer )% mod;
-	       p[i] = answer;
-	       
-
-	       
-	   }
-	   
-	   int ans = 0;
-	   for(int i =0;i <= n; i++){
-	       ans = (ans + p[i])% mod;
-	   }
-	   
-	   for(int i = 1;i <= n ;i++){
-	       int x;
-	       if(i-2 >= 0){
-	           x = p[i-2];
-	       }
-	       else{
-	           x = 1;
-	       }
-	       int y = (power(mp[i])-1+mod)%mod;
-	       int z = (1LL * x * y) % mod;
-	       ans = (ans + z)% mod;
-	   }
-	   cout << ans << endl;
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
+	    sort(arr.begin(),arr.end());
+	    ll ans = 0;
+	    
+	    for(int max_pos = 0; max_pos < n; max_pos++){
+	        memset(dp,-1,sizeof(dp));
+	        ans = (ans+f(0,0,max_pos,arr))% mod;
+	    }
+	    cout<< ans << "\n";
+	    
 	}
 
 }
