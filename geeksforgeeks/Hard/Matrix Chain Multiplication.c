a027d40c877e5c7d8a68a48e18162377c5c8a431
Shikhar <shikhar0538@gmail.com>
2026-08-22 20:34:14 +0530

    Add Matrix Chain Multiplication (Hard)

--- geeksforgeeks/Hard/Matrix Chain Multiplication.c
+++ geeksforgeeks/Hard/Matrix Chain Multiplication.c
@@ -1,36 +1,31 @@
 class Solution {
   public:
-  
-    int f(int i, int j, vector<int>&arr,vector<vector<int>>&dp){
-        if(i == j){
-            return 0;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        int mini = 1e9;
-        
-        for(int k = i; k < j; k++){
-            int ops = arr[i-1] * arr[k] * arr[j] + f(i,k,arr,dp)+ f(k+1,j,arr,dp);
-            mini = min(mini,ops);
-            
-        }
-        return dp[i][j] = mini;
-        
-    }
     int matrixMultiplication(vector<int> &arr) {
         // code here
-        // find most effecient way to multiply
-        // the matrixes are
-        // 2 x 1
-        // 1 x 3
-        // 3 x 4
-        // least number of mutliplications
         int n= arr.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return f(1,n-1,arr,dp);
+        vector<vector<int>>dp(n,vector<int>(n,0));
         
-        
+        for(int len = 2; len <= n-1; len++){
+            
+            // for each length find starting point
+            // and for each starting find ending point
+            // partition that 
+            
+            // portition for all possible k
+            
+            for(int i = 1;i + len -1 <= n-1; i++){
+                
+                int j = i+ len-1;
+                dp[i][j] = 1e9;
+                
+                
+                for(int k = i; k < j ; k++){
+                    int ops = arr[i-1] * arr[k] * arr[j] + dp[i][k] + dp[k+1][j];
+                    dp[i][j] = min(dp[i][j],ops);
+                }
+            }
+        }
+        return dp[1][n-1];
         
     }
 };
