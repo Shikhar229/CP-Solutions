a5933d51104cbc65b6f5c9cb3f8fc94348e24769
Shikhar <shikhar0538@gmail.com>
2026-08-22 12:16:11 +0530

    Add Game with Integers (800)

---
+++ codeforces/800/1899A - Game with Integers.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+const ll mod = 1e9 + 7;
+const ll INF = 1000000000000000000LL;
+
+ll binpow(ll a, ll b) {
+    ll ans = 1;
+
+    while (b > 0) {
+        if (b & 1)
+            ans = (ans * a) % mod;
+
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+
+    return ans;
+}
+
+bool isPrime(ll n) {
+    if (n < 2) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
+
+    for (ll i = 3; i <= n / i; i += 2) {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    if(n% 3 == 0){
+        cout << "Second\n";
+    }
+    else{
+        cout << "First\n";
+    }
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
