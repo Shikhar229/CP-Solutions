ddfed91c49afdc5d30895b802abfd539cacec85a
Shikhar <shikhar0538@gmail.com>
2026-08-23 23:49:01 +0530

    Add Team (800)

---
+++ codeforces/800/231A - Team.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int ans = 0;
+    while(n != 0){
+        vector<int>vec(3);
+        int count = 0;
+        for(int i = 0;i < 3; i++){
+            cin >> vec[i];
+            if(vec[i])count++;
+        }
+        // cout << count << endl;
+        if(count  >=2){
+            ans++;
+        }
+        n--;
+    }
+    cout << ans << endl;
+}
