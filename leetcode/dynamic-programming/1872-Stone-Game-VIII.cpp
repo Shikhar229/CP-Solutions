03f8d30d9db92e96288472201d507908701870d4
Shikhar <shikhar0538@gmail.com>
2026-08-24 23:14:34 +0530

    Add Stone-Game-VIII (dynamic-programming)

---
+++ leetcode/dynamic-programming/1872-Stone-Game-VIII.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    int stoneGameVIII(vector<int>& stones) {
+        int n = stones.size();
+        vector<int>prefix(n,0);
+        prefix[0] = stones[0];
+        for(int i = 1;i < n; i++){
+
+            prefix[i] = prefix[i-1]+stones[i];
+
+        }
+        vector<int>dp(n,0);
+        dp[n-1] = prefix[n-1];
+
+        for(int i = n-2;i >= 1 ; i--){
+            int pick = prefix[i]- dp[i+1];
+            int skip  = dp[i+1];
+            dp[i] = max(skip,pick);
+
+
+        }
+        return dp[1];
+
+
+
+
+        
+        
+
+
+
+        
+        
+    }
+};
