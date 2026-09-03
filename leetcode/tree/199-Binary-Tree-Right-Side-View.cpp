652ea14965c76c83ecb132d1a6fbda62cbff11bc
Shikhar <shikhar0538@gmail.com>
2026-09-04 01:00:06 +0530

    Add Binary-Tree-Right-Side-View (tree)

---
+++ leetcode/tree/199-Binary-Tree-Right-Side-View.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    map<int,int>mp;
+    void traverse(TreeNode* root, int height){
+        if(root == NULL) return;
+        if(mp.find(height) == mp.end()){
+            mp[height] = root->val;
+
+        }
+        traverse(root->right,height+1);
+        traverse(root->left,height+1);
+
+        
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        traverse(root,0);
+        vector<int>ans;
+        for(auto it: mp){
+            ans.push_back(it.second);
+
+
+        }
+        return ans;
+
+    }
+};
