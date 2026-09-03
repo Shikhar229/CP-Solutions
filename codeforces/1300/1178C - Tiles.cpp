ab6fea7e8250584c175d42cc2d9a1fc87a53fb72
Shikhar <shikhar0538@gmail.com>
2026-09-03 23:28:35 +0530

    Add Tiles (1300)

---
+++ codeforces/1300/1178C - Tiles.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+const ll mod =998244353;
+const ll INF = 1000000000000000000LL;
+
+ll binpow(ll a, ll b) {
+    ll ans = 1;
+
+    while (b > 0) {
+        if (b & 1)
+            ans = (ans * a) % mod;
+
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+
+    return ans;
+}
+
+bool isPrime(ll n) {
+    if (n < 2) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
+
+    for (ll i = 3; i <= n / i; i += 2) {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+void solve() {
+    int w,h;
+    cin >> w >> h;
+    cout << binpow(2,w+h)<< endl;;
+
+    
+    
+
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // int t;
+    // cin >> t;
+    // while (t--) {
+    //     solve();
+    // }
+
+    solve();
+
+    return 0;
+}
