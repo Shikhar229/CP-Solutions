e0f2561e588fdb904870601d7ef5c661ac66deda
Shikhar <shikhar0538@gmail.com>
2026-09-04 18:07:05 +0530

    Add Longest Divisors Interval (900)

---
+++ codeforces/900/1855B - Longest Divisors Interval.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+const ll mod = 1e9 + 7;
+const ll INF = 1000000000000000000LL;
+
+ll binpow(ll a, ll b) {
+    ll ans = 1;
+
+    while (b > 0) {
+        if (b & 1)
+            ans = (ans * a) % mod;
+
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+
+    return ans;
+}
+
+bool isPrime(ll n) {
+    if (n < 2) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
+
+    for (ll i = 3; i <= n / i; i += 2) {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+void solve() {
+    long long n;
+    cin >> n;
+    if(n% 2 ==1){
+      cout << 1 << "\n";
+    }
+    else{
+      int i = 1;
+      int j = 1;
+      while(n% j == 0){
+        j++;
+      }
+      int len = ((j-1)-i)+1;
+      cout << len << "\n";
+
+    }
+    
+
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    // solve();
+
+    return 0;
+}
