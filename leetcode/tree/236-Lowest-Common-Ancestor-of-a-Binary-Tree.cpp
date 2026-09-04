343bc21fdcc2577fd13dd5b7ebf4137d595460c8
Shikhar <shikhar0538@gmail.com>
2026-09-05 01:39:58 +0530

    Add Lowest-Common-Ancestor-of-a-Binary-Tree (tree)

---
+++ leetcode/tree/236-Lowest-Common-Ancestor-of-a-Binary-Tree.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+
+
+        if(root == NULL){
+            return NULL;
+        }
+
+        if(root == p){
+            return p;
+
+        }
+        if(root == q){
+            return q;
+        
+        }
+
+
+        TreeNode* left = lowestCommonAncestor(root->left,p,q);
+        TreeNode* right = lowestCommonAncestor(root->right,p,q);
+        
+        if(right != NULL && left  != NULL){
+            return root;
+            // cout << left->val << endl;
+            // cout << right->val << endl;
+        }
+
+        if(left == NULL) {
+            return right;
+        
+        }
+        if(right == NULL){
+            return left;
+        }
+        return NULL;
+
+        
+        
+    }
+};
