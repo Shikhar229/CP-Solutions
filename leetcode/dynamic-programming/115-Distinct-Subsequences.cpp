35a69804adb52fe87f3e7e9ad5ebad0d2bfd6993
Shikhar <shikhar0538@gmail.com>
2026-09-06 22:05:26 +0530

    Add Distinct-Subsequences (dynamic-programming)

--- leetcode/dynamic-programming/115-Distinct-Subsequences.cpp
+++ leetcode/dynamic-programming/115-Distinct-Subsequences.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    int solve(int i, int j , string s, string t,vector<vector<int>>& dp){
-
-        if(j == t.size()){
-            return 1;
-        }
-        if(i == s.size()){
-            return 0;
-        }
-        
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-
-        if(s[i] == t[j]){
-            return  dp[i][j] = solve(i+1, j+1, s,t,dp) + solve(i+1, j ,s,t,dp);
-            
-        }
-        return dp[i][j] = solve(i+1,j,s,t,dp);
-    }
     int numDistinct(string s, string t) {
-        int n = s.size();
-        int m = t.size();
-        dp.resize(n,vector<int>(m,-1));
-        
-        int ans = solve(0,0,s,t,dp);
-        return ans;
-        
+
+        int m = s.size();
+        int n = t.size();
+        vector<vector<unsigned long long >>dp(m+1,vector<unsigned long long>(n+1,0));
+       
+        for(int i = 0; i  <= m ;i++){
+           dp[i][n] = 1;
+        }
+
+        for(int i = m-1; i>= 0; i--){
+            for(int j = n-1; j >= 0;j--){
+                if(s[i] == t[j]){
+                    dp[i][j] = dp[i+1][j+1] + dp[i+1][j];
+                }
+                else{
+                    dp[i][j] = dp[i+1][j];
+                }
+            }
+        }
+        return dp[0][0];
+    
+  
     }
 };
