11aafed84b3fda4f24082923430493174c17456f
Shikhar <shikhar0538@gmail.com>
2026-09-07 21:38:28 +0530

    Add The Legend of Freya the Frog (1100)

---
+++ codeforces/1100/2009C - The Legend of Freya the Frog.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+const ll mod = 1e9 + 7;
+const ll INF = 1000000000000000000LL;
+
+ll binpow(ll a, ll b) {
+    ll ans = 1;
+
+    while (b > 0) {
+        if (b & 1)
+            ans = (ans * a) % mod;
+
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+
+    return ans;
+}
+
+bool isPrime(ll n) {
+    if (n < 2) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
+
+    for (ll i = 3; i <= n / i; i += 2) {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+void solve() {
+    ll x,y;
+    cin>>  x >> y;
+    ll k;
+    cin >> k;
+
+    ll forx  = (x+k-1)/k;
+    ll fory = (y+k-1)/k;
+    
+    if(forx > fory){
+        cout <<2 * (forx)-1<< "\n";
+    }
+    else{
+        cout << 2 * fory << "\n";
+        
+    }
+        
+    
+    
+    
+    
+
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    // solve();
+
+    return 0;
+}
