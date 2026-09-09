d3c6cd751b79d7ee406811233d1f8ca34faedc03
Shikhar <shikhar0538@gmail.com>
2026-09-09 07:43:00 +0530

    Add Count-Commas-in-Range-II (math)

--- leetcode/math/3871-Count-Commas-in-Range-II.cpp
+++ leetcode/math/3871-Count-Commas-in-Range-II.cpp
@@ -3,15 +3,11 @@ public:
     #define ll long long
 
     long long countCommas(long long n) {
-
         if(n <= 999){
             return 0;
         }
-
         ll i = 1;
         ll multiply = 1000;
-        
-
         ll count = 0;
         while(n >= multiply){
             ll r = min(n, multiply *1000 -1);
@@ -24,9 +20,6 @@ public:
         }
         return count;
        
-
-       
-
       
     }
 };
