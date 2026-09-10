42f2f78ad790777c477eda50427a38d57fb88d3e
Shikhar <shikhar0538@gmail.com>
2026-09-10 23:41:59 +0530

    Add Count-Nodes-Equal-to-Average-of-Subtree (tree)

---
+++ leetcode/tree/2265-Count-Nodes-Equal-to-Average-of-Subtree.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int count = 0;
+    pair<int,int>traverse(TreeNode* root){
+        if(root == NULL) return {0,0};
+        auto left = traverse(root->left);
+        auto right = traverse(root->right);
+
+        int sum = root->val + left.first + right.first;
+        int c = left.second + right.second + 1;
+
+        if(sum/c == root->val){
+            count++;
+
+        }
+
+        return {sum,c};
+
+        
+    }
+    int averageOfSubtree(TreeNode* root) {
+        traverse(root);
+        return count;
+
+
+        
+    }
+};
